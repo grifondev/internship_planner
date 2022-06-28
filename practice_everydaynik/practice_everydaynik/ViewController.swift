d1a219b8140fb2ea02822b18768a8866aea52da4
Dmitriy <70837779+grifondev@users.noreply.github.com>
2022-06-28 12:40:28 +0300

    v 0.01 interface and 1 button (that works!!!)
    read title

---
+++ practice_everydaynik/practice_everydaynik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice_everydaynik
+//
+//  Created by Дмитрий Мицкевич on 27.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var l01: UILabel!
+    @IBAction func button01(_ sender: UIButton) {
+        l01.text = "zzzzzzzzz"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
